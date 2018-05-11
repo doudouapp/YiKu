25707521e250d8d7819b273dc72e54146d207435
dangshuai <dangshuai@tyrbl.com>
2018-05-11 16:12:10 +0800

    我的

---
+++ YiKu/Home/YKHomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  YKHomeViewController.swift
+//  YiKu
+//
+//  Created by tyrbl on 2018/4/3.
+//  Copyright © 2018年 dangshuai. All rights reserved.
+//
+
+import UIKit
+
+class YKHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "YiKu"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
